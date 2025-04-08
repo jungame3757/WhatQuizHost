53960480cf3899d0a6ccbc8a97d26cc8c950781d
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-08 17:40:43 +0900

    플레이어 아이콘 연결

--- Build/firebasetest.framework.js
+++ Build/firebasetest.framework.js
@@ -1287,10 +1287,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7562208: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7562263: ($0) => { performance.now = function() { return $0; }; },  
- 7562311: ($0) => { performance.now = function() { return $0; }; },  
- 7562359: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7562368: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7562423: ($0) => { performance.now = function() { return $0; }; },  
+ 7562471: ($0) => { performance.now = function() { return $0; }; },  
+ 7562519: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
