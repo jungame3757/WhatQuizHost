73143124d4a2c9d8640d4ef32c04a12d2e6ff2aa
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-10 20:12:30 +0900

    세션 중복 해결 재시도

--- Build/firebasetest.loader.js
+++ Build/firebasetest.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94934842,
+    wasmFileSize: 94935293,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
