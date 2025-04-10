cfd17cb5e9f585598b7a43985bb1527f33994d7d
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-10 18:06:41 +0900

    세션 생성 오류 해결

--- Build/firebasetest.loader.js
+++ Build/firebasetest.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94934393,
+    wasmFileSize: 94934842,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
