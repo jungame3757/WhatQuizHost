b170c0d222edaee0a699d4f8f813922fe6b73829
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-18 15:16:54 +0900

    id 인식문제 개선

--- Build/AuthTokenTest.loader.js
+++ Build/AuthTokenTest.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94956872,
+    wasmFileSize: 94956918,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
