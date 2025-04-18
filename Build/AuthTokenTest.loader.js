b3fd385d53a55f54c38b03837a507fbd271eab01
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-18 15:27:34 +0900

    시리얼라이즈 문제 개선

--- Build/AuthTokenTest.loader.js
+++ Build/AuthTokenTest.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 94956918,
+    wasmFileSize: 94958105,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
