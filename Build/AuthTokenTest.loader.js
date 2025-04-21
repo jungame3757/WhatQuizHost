1aea830c1890b2454ccb4f84a2327ba9947b63b6
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-21 20:26:37 +0900

    패널 관리 개선

--- Build/AuthTokenTest.loader.js
+++ Build/AuthTokenTest.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95070670,
+    wasmFileSize: 95070683,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
