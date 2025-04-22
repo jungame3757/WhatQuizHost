a3faa50fd8d8f5dd150a8d8eeab6d6da96dd3cde
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-22 10:01:10 +0900

    세션데이터 디버깅

--- Build/AuthTokenTest.loader.js
+++ Build/AuthTokenTest.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95073948,
+    wasmFileSize: 95074081,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
