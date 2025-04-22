c9981bd793f2d84300fb2f3f1b9fb942c2e3454c
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-22 09:42:28 +0900

    게임 규칙 ui 개선

--- Build/AuthTokenTest.framework.js
+++ Build/AuthTokenTest.framework.js
@@ -1283,10 +1283,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7713952: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7714007: ($0) => { performance.now = function() { return $0; }; },  
+  7714000: () => { Module['emscripten_get_now_backup'] = performance.now; },  
  7714055: ($0) => { performance.now = function() { return $0; }; },  
- 7714103: () => { performance.now = Module['emscripten_get_now_backup']; }
+ 7714103: ($0) => { performance.now = function() { return $0; }; },  
+ 7714151: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/AuthTokenTest.loader.js
+++ Build/AuthTokenTest.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95073896,
+    wasmFileSize: 95073948,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
