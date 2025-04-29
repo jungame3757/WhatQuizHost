5b2f986148218d05f890191ccf7cf684905d4ddf
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-29 19:46:30 +0900

    2차 재작업

--- Build/WhatQuizHost.framework.js
+++ Build/WhatQuizHost.framework.js
@@ -1283,10 +1283,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7886096: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7886151: ($0) => { performance.now = function() { return $0; }; },  
+  7886144: () => { Module['emscripten_get_now_backup'] = performance.now; },  
  7886199: ($0) => { performance.now = function() { return $0; }; },  
- 7886247: () => { performance.now = Module['emscripten_get_now_backup']; }
+ 7886247: ($0) => { performance.now = function() { return $0; }; },  
+ 7886295: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/WhatQuizHost.loader.js
+++ Build/WhatQuizHost.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 99081262,
+    wasmFileSize: 99086161,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
