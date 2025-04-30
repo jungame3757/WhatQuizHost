aeed55290a1a1c5698ed75ef531b89485e414d81
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-30 10:45:13 +0900

    디버깅 로그

--- Build/WhatQuizHost.loader.js
+++ Build/WhatQuizHost.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 99084390,
+    wasmFileSize: 99082845,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
