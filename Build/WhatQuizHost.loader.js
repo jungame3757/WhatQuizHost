876be64cee8ca41399bb863d94875205b8e17e52
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-30 15:08:24 +0900

    고침!

--- Build/WhatQuizHost.loader.js
+++ Build/WhatQuizHost.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 99085970,
+    wasmFileSize: 99086068,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
