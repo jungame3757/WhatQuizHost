b10ca00ccd09de5c7301403540ddd08f60cf666f
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-30 14:31:48 +0900

    일부 버그 픽스

--- Build/WhatQuizHost.loader.js
+++ Build/WhatQuizHost.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 99085973,
+    wasmFileSize: 99085970,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
