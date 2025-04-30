6a12a6f218eb5ae04d7f5d47e0bcd3b89ae32e9c
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-04-30 16:39:41 +0900

    최종결과 빼고 완성

--- Build/WhatQuizHost.framework.js
+++ Build/WhatQuizHost.framework.js
@@ -1283,10 +1283,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7886528: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7886583: ($0) => { performance.now = function() { return $0; }; },  
- 7886631: ($0) => { performance.now = function() { return $0; }; },  
- 7886679: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7886592: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7886647: ($0) => { performance.now = function() { return $0; }; },  
+ 7886695: ($0) => { performance.now = function() { return $0; }; },  
+ 7886743: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
