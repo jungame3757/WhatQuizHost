196593ee33689c85c9eb1961868b7d1e8f96f153
jungame3757 <72293731+jungame3757@users.noreply.github.com>
2025-05-07 10:48:51 +0900

    게임오버 설정

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-WhatQuizSDK-1.0";
 const contentToCache = [
-    "Build/488fa2001e99cb3a5658f84c2164fb4e.loader.js",
+    "Build/6831d549f49b90ae95cef97ca792fb94.loader.js",
     "Build/1678a157067e2292d53ce0bffc7ea155.framework.js",
-    "Build/682679f4243efc5e592a86b5c12ee87b.data",
-    "Build/e6572084eab69348230f429a1e26e085.wasm",
+    "Build/243b51e318896b2bed70bbd7ca97e35c.data",
+    "Build/a8ea4e28e6a0bbb766d57a0bd889f0f4.wasm",
     "TemplateData/style.css"
 
 ];
